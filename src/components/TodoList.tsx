001d332e3cfd9286696287583cec5682a7a67f5b
volhaf <Volha.f@outlook.com>
2024-07-09 14:58:29 +0200

    add todolist component

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,24 @@
+
+
+
+
+export function TodoList() {
+    return (
+        <div className="todolist">
+            <h3></h3>
+            <input/>
+            <button>+</button>
+            <ul>
+                <li>
+                    <input/>
+                    <span>text</span>
+                    <button>+</button>
+                    <button>!</button>
+                </li>
+            </ul>
+            <button>All</button>
+            <button>Active</button>
+            <button>Done</button>
+        </div>
+    )
+}
