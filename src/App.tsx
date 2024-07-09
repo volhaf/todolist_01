801790e2b1f640805fd24af1c7d38ff4c29e4a6c
volhaf <Volha.f@outlook.com>
2024-07-09 15:58:24 +0200

    add global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
 import {TodoList} from "./components/TodoList";
 import {Header} from "./components/Header";
 
 export type TaskType = {
-    id: string,
+    id: number,
     title: string,
     isDone: boolean,
 }
 function App() {
+    const [tasks, setTasks] = useState<TaskType[]>([
+        {id: 1, title: 'Task 1', isDone: false},
+    ]);
   return (
     <div className="App">
         <Header/>
