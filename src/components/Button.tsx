5043bc40df943b8cb02dc85782568faaa5c66dd8
volhaf <Volha.f@outlook.com>
2024-07-11 16:52:08 +0200

    typing button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,14 +3,16 @@ type ButtonProps = {
     onClick: () => void;
     title: string;
     disabled?: boolean;
+    className?: string;
 
 }
 
-export function Button({onClick,title, disabled}: ButtonProps) {
+export function Button({onClick,title, disabled, className}: ButtonProps) {
     return (
         <button
             onClick={onClick}
             disabled={disabled}
+            className={className}
         > {title}
         </button>
     )
